2ca0f2f532ce6712501ef743eae531058ce62606
欧阳毅军 <oyangyijun@iicall.com>
2022-08-15 18:16:40 +0800

    CountDownLatch demo

--- app/src/main/java/com/ouyang/androidstudytree/MainActivity.kt
+++ app/src/main/java/com/ouyang/androidstudytree/MainActivity.kt
@@ -1,34 +1,45 @@
 package com.ouyang.androidstudytree
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import com.ouyang.androidstudytree.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.drakeet.multitype.MultiTypeAdapter
+import com.ouyang.androidstudytree.binder.MainBinder
+import com.ouyang.lib_common.annotation.AnnotationMain
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var multiTypeAdapter: MultiTypeAdapter? = null
 
-    private lateinit var binding: ActivityMainBinding
+    val list = ArrayList<Any>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+        setContentView(R.layout.activity_main)
+        addData()
+        multiTypeAdapter = MultiTypeAdapter(list)
+        var binder = MainBinder()
+        binder.setOnItemClickListener { view, item ->
+            when (item) {
+                AnnotationMain.JNI -> {
+                    startActivity(Intent(this@MainActivity, JNIActivity::class.java))
+                }
+                AnnotationMain.COUNTDOWNLATCH -> {
+                    startActivity(Intent(this@MainActivity, CountDownLatchActivity::class.java))
+                }
+            }
+        }
+        multiTypeAdapter?.register(String::class, binder)
+        var manager = LinearLayoutManager(this)
+        manager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = manager
+        recyclerView.adapter = multiTypeAdapter
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    companion object {
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
+    private fun addData() {
+        list.add(AnnotationMain.JNI)
+        list.add(AnnotationMain.COUNTDOWNLATCH)
     }
 }
---
+++ app/src/main/java/com/ouyang/androidstudytree/binder/MainBinder.kt
@@ -0,0 +1,38 @@
+package com.ouyang.androidstudytree.binder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.ouyang.androidstudytree.R
+import com.ouyang.lib_common.BaseItemViewBinder
+
+class MainBinder : BaseItemViewBinder<String, MainBinder.ViewHolder>(), View.OnClickListener {
+    override fun onCreateViewHolder(
+        inflater: LayoutInflater,
+        parent: ViewGroup
+    ): MainBinder.ViewHolder {
+        return ViewHolder(inflater.inflate(R.layout.binder_main, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MainBinder.ViewHolder, item: String) {
+        holder.btn.text = item
+        holder.btn.tag = item
+    }
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var btn = itemView.findViewById<Button>(R.id.btn)
+
+        init {
+            btn.setOnClickListener(this@MainBinder)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        onItemClickListener?.let {
+            onItemClickListener.OnItemClick(v, v?.tag)
+        }
+    }
+}
